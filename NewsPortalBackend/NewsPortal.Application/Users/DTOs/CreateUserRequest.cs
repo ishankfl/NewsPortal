2473925394139cb951f972e36f0e3f8759ab500f
ishankfl <ishankafle5@gmail.com>
2025-07-27 22:19:57 +0545

    user-dtos-completed

---
+++ NewsPortalBackend/NewsPortal.Application/Users/DTOs/CreateUserRequest.cs
@@ -0,0 +1,17 @@
+﻿using NewsPortal.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Application.Users.DTOs
+{
+    public class CreateUserRequest
+    {
+        public string Username { get; set; } = default;
+        public string Email { get; set; } = default;
+        public string PlainPassword { get; set; }   
+        public Role Role { get; set; }
+    }
+}
---
+++ NewsPortalBackend/NewsPortal.Application/Users/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using NewsPortal.Application.Users.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Application.Users.Interfaces
+{
+    public interface IUserService
+    {
+        Task<int> CreateAsync(CreateUserRequest request, bool autoGeneratePassword = true);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> SuspendAsync(int id);
+        Task<bool> UnsuspendAsync(int id);
+        Task<UserResponse?> GetByIdAsync(int id);
+        Task<IEnumerable<UserResponse>> GetAllAsync();
+        Task<bool> UpdateRoleAsync(int id, Role newRole);
+    }
+}
