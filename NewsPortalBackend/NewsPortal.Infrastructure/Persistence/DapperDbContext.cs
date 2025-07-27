eaaed09958c6e0990b89a8a62dd688894cc19d13
ishankfl <ishankafle5@gmail.com>
2025-07-27 15:36:22 +0545

    dapper-added

---
+++ NewsPortalBackend/NewsPortal.Infrastructure/Persistence/DapperDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Infrastructure.Persistence
+{
+    public class DapperDbContext
+    {
+        private readonly string _connectionString;
+
+        public DapperDbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")
+                               ?? throw new ArgumentNullException(nameof(configuration), "Connection string not found.");
+        }
+
+        public NpgsqlConnection CreateConnection() => new NpgsqlConnection(_connectionString);
+    }
+}
