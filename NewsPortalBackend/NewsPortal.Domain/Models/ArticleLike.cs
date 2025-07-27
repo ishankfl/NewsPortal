97e3b3c0e1e4e3609b47e780e3407bb5c8629d8c
ishankfl <ishankafle5@gmail.com>
2025-07-27 15:03:33 +0545

    comment-model-added

---
+++ NewsPortalBackend/NewsPortal.Domain/Models/ArticleLike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Models
+{
+    public class ArticleLike
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Article Article { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ NewsPortalBackend/NewsPortal.Domain/Models/ArticleView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Models
+{
+    public class ArticleView
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public string UserIp { get; set; }
+        public DateTime ViewedAt { get; set; }
+
+        public Article Article { get; set; }
+    }
+}
