020d5943e9f45ac98e67b8037a89c8b07f29ff07
ishankfl <ishankafle5@gmail.com>
2025-07-27 14:57:56 +0545

    advertisement-model-added

---
+++ NewsPortalBackend/NewsPortal.Domain/Models/Advertisement.cs
@@ -0,0 +1,26 @@
+﻿using NewsPortal.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Models
+{
+    public class Advertisement
+    {
+        public int Id { get; set; }
+        public string Title_En { get; set; } = default;
+        public string Title_Np { get; set; } = default;
+        public string ImageUrl { get; set; } = default;
+        public string RedirectUrl { get; set; }
+        public string PlacementKey { get; set; } = default;
+        public AdPlacementType PlacementType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public User Creator { get; set; }
+    }
+}
