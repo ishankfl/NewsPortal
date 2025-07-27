acc1374b8816fe5d86e62d29db1771191db2eb4d
ishankfl <ishankafle5@gmail.com>
2025-07-27 14:39:47 +0545

    user-model-created

---
+++ NewsPortalBackend/NewsPortal.Domain/Enums/AdPlacementType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Enums
+{
+    public enum AdPlacementType
+    {
+        header,
+        banner,
+        sidebar,
+        left_sidebar,
+        right_sidebar,
+        middle_news,
+        bottom_news
+    }
+}
---
+++ NewsPortalBackend/NewsPortal.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using NewsPortal.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+        public Role Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
