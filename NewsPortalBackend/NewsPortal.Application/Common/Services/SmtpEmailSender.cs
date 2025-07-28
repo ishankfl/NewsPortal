8c2e54bc450a3d9a6fdc4f961b124a1735455354
ishankfl <ishankafle5@gmail.com>
2025-07-28 11:17:03 +0545

    smtp-email-sender-added

---
+++ NewsPortalBackend/NewsPortal.Application/Common/Services/SmtpEmailSender.cs
@@ -0,0 +1,38 @@
+﻿using NewsPortal.Application.Common.Interfaces;
+using NewsPortal.Application.Users.DTOs;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Application.Common.Services
+{
+    public class SmtpEmailSender : IEmailSender
+    {
+        private readonly SmtpSettings _smtpSettings;
+
+        public SmtpEmailSender(SmtpSettings smtpSettings)
+        {
+            _smtpSettings = smtpSettings;
+        }
+
+        public async Task SendAsync(string to, string subject, string htmlBody)
+        {
+            using (var message = new MailMessage())
+            {
+                message.From = new MailAddress(_smtpSettings.SenderEmail, _smtpSettings.SenderName);
+                message.Subject = subject;
+                message.Body = htmlBody;
+                message.IsBodyHtml = true;
+                message.To.Add(to);
+
+                using (var client = new SmtpClient(_smtpSettings.Host, _smtpSettings.Port))
+                {
+                    client.EnableSsl = _smtpSettings.EnableSsl;
+                    client.Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password);
+                    await client.SendMailAsync(message);
+                }
+            }
+        }
+
+    }
+}
---
+++ NewsPortalBackend/NewsPortal.Application/Users/DTOs/SmtpSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Application.Users.DTOs
+{
+    public class SmtpSettings
+    {
+        public string Host { get; set; } = default;
+        public int Port { get; set; }
+        public bool EnableSsl { get; set; }
+        public string SenderEmail { get; set; } = default;
+        public string SenderName { get; set; } = "News Portal";
+        public string Username { get; set; } = default;
+        public string Password { get; set; } = default;
+    }
+
+}
