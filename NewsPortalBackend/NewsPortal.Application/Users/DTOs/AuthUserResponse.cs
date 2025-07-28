cc99fb11f7a6aa20cbeb629f4c80019a12dbd546
ishankfl <ishankafle5@gmail.com>
2025-07-28 09:43:34 +0545

    user-dto-created

--- NewsPortalBackend/NewsPortal.Application/Users/DTOs/AuthUserResponse.cs
+++ NewsPortalBackend/NewsPortal.Application/Users/DTOs/AuthUserResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsPortal.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace NewsPortal.Application.Users.DTOs
 {
-    internal interface CreateUserRequest
+    public class AuthUserResponse
     {
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public Role Role { get; set; }
+        public string Token { get; set; } = default!;
     }
 }
--- NewsPortalBackend/NewsPortal.Application/Users/DTOs/UserResponse.cs
+++ NewsPortalBackend/NewsPortal.Application/Users/DTOs/UserResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsPortal.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace NewsPortal.Application.Users.DTOs
 {
-    internal interface CreateUserRequest
+
+    public class UserResponse
     {
+        public int Id { get; set; }
+        public string Username { get; set; } = default;
+        public string Email { get; set; } = default;
+        public Role Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsSuspended { get; set; }
+        public DateTime? SuspendedAt { get; set; }
     }
 }
