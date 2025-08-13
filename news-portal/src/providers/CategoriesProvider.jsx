695e16e28fe02c8f51f2e5ecfd4e1d56d0b44b17
ishankfl <ishankafle5@gmail.com>
2025-08-13 11:26:07 +0545

    category-provider-added

---
+++ news-portal/src/providers/CategoriesProvider.jsx
@@ -0,0 +1,28 @@
+// CategoriesProvider.jsx
+import React, { createContext, useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getCategories } from "../../api/category-services";
+
+const CategoriesContext = createContext({
+  categories: [],
+  isLoading: false,
+  isError: false,
+  error: null,
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const { data: categories = [], isLoading, isError, error } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories,
+    staleTime: 1000 * 60 * 10,  // 10 min
+    cacheTime: 1000 * 60 * 60,  // 1 hour
+  });
+
+  return (
+    <CategoriesContext.Provider value={{ categories, isLoading, isError, error }}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
+
+export const useCategories = () => useContext(CategoriesContext);
